40e2f6432f87242aac75ab0d20bbd47bb1ffa210
rabbitfighter81 <rabbitfighter81@gmail.com>
2018-05-21 16:52:36 -0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  txt = txt;
+  author = author;
+
+  constructor() { }
+
 }
+
+const txt = 'A dreamer is one who can only find his way by moonlight' +
+  ' and his punishment is that he sees the dawn before the rest of the world.';
+const author = 'Oscar Wilde';
